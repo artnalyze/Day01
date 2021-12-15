1d87ed115a51b92d8b8328d7025b322c32d5af71
Artnalyze <artnalyze@gmail.com>
2021-12-15 23:28:45 +0700

    update readtodo

--- BasicTypescript/readtodo.ts
+++ BasicTypescript/readtodo.ts
@@ -1,1 +1,25 @@
-import axios from 'axios'
+import axios from "axios";
+
+// กำหนด url สำหรับอ่าน API
+const url = "https://jsonplaceholder.typicode.com/todos/5";
+
+// กำหนด Interface เพื่อไว้กำหนดรูปแบบตัวแปร
+// interface, type
+interface Todo {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+axios.get(url).then((res) => {
+  const todo: Todo = res.data;
+
+  const id = todo.id;
+  const title = todo.title;
+  const userId = todo.userId
+  const completed = todo.completed
+
+  // console.log(todo);
+  console.log({id,title,userId,completed});
+});
