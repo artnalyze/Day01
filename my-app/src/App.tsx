45df2c00547a2ff2a41da1eb39559af5a080c0c4
Artnalyze <artnalyze@gmail.com>
2021-12-17 20:38:14 +0700

    config tailwind

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello Type Script</h1>
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div className="columns-2">
+        <p>Well, let me tell you something, ...</p>
+        <p className="bg-lime-300">Sure, go ahead, laugh...</p>
+        <p className="bg-indigo-800">Maybe we can live without...</p>
+        <p>Look. If you think this is...</p>
+      </div>
     </div>
   );
 }
